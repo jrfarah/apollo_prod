2aeef36f19ebb91faf63c27b4145654092962248
jrfarah <jrfarah1999@gmail.com>
2018-03-18 00:40:35 -0400

    added license

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'apollo',
   packages = ['apollo'], # this must be the same as the name above
-  version = '0.5',
+  version = '0.1',
   description = 'sklearn wrapper',
   author = 'Joseph Farah',
   author_email = 'jrfarah1999@gmail.com',
-  url = 'https://github.com/jrfarah/apollo', # use the URL to the github repo
-  download_url = 'https://github.com/jrfarah/apollo/archive/0.5.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/jrfarah/apollo_prod', # use the URL to the github repo
+  download_url = 'https://github.com/jrfarah/apollo_prod/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['machine', 'learning', 'sklearn', 'wrapper', 'simplicity', 'apollo'], # arbitrary keywords
   classifiers = [],
 )
