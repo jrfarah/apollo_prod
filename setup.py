351f0311648a440c8d0bd20ca60f9ed83fe140ad
jrfarah <jrfarah1999@gmail.com>
2018-03-19 00:46:04 -0400

    removed unnecessary folders

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'apollo_ml',
   packages = ['apollo_ml'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'sklearn wrapper',
   author = 'Joseph Farah',
   author_email = 'jrfarah1999@gmail.com',
   url = 'https://github.com/jrfarah/apollo_prod', # use the URL to the github repo
-  download_url = 'https://github.com/jrfarah/apollo_prod/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/jrfarah/apollo_prod/archive/0.2.tar.gz', # I'll explain this in a second
   keywords = ['machine', 'learning', 'sklearn', 'wrapper', 'simplicity', 'apollo_ml'], # arbitrary keywords
   classifiers = [],
 )
